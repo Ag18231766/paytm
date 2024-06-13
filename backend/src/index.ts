6eb3c3b6bccf95524067d18a683cd1372269d87a
Ag18231766 <amartyagupta18231766@gmail.com>
2024-06-13 18:28:46 +0530

    corrected the rootdir and outdir

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { Router } from './routes';
+import {connect} from 'mongoose';
 const app = express();
 
+const dbUrl = 'mongodb://localhost:27017/paytm';
 app.use('/api/v1',Router);
 
+
+
+
+connect(dbUrl).then(() => console.log('connected to db')).catch((err) => console.log("can't connect to db"));
 app.listen(5000,() => {
     console.log('running on 5000');
 })
